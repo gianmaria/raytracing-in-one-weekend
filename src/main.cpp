c45359f74ccb9535365694cf735eaaf5dc233bcf
Gianmaria <gianmaria.bonici@gmail.com>
2021-07-08 08:03:24 +0200

    Final scene

--- src/main.cpp
+++ src/main.cpp
@@ -54,8 +54,29 @@ int main1(void)
 
 struct World
 {
-    Sphere spheres[5];
+    Sphere spheres[1024] = {};
+    size_t sphere_count = 0;
 };
+
+World global_world;
+
+
+static bool add_sphere_to_world(World* world, const Sphere* new_sphere)
+{
+    unsigned max_spheres = ArrayCount(world->spheres);
+
+    if (world->sphere_count < max_spheres)
+    {
+        Sphere* sphere = &world->spheres[world->sphere_count];
+        *sphere = *new_sphere;
+        ++world->sphere_count;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 static void write_color(FILE* fp, Color pixel_color, int samples_per_pixel)
 {
@@ -87,7 +108,7 @@ static bool ray_hit_object_in_world(
 
     // check spheres
     for (size_t i = 0;
-        i < ArrayCount(world->spheres);
+        i < world->sphere_count;
         ++i)
     {
         const Sphere* sphere = &world->spheres[i];
@@ -134,21 +155,139 @@ static Color ray_color(
 }
 
 
-static bool done_rendering = false;
+static bool g_done_rendering = false;
+static int g_total_scanlines = 0;
+static int g_scanlines_processed = 0;
 
 static void print_progress()
 {
     using namespace std::chrono_literals;
 
-    while (!done_rendering)
+    chrono::time_point<chrono::steady_clock> start = chrono::steady_clock::now();
+
+    printf("Begin rendering\n");
+
+    while (!g_done_rendering)
     {
-        printf(".");
+        float perc = (float)g_scanlines_processed / (float)g_total_scanlines * 100.0f;
+        auto now = chrono::steady_clock::now();
+        long long elapsed_ms = 
+            chrono::duration_cast<chrono::milliseconds>(now - start).count();
+
+        long long sec = elapsed_ms / 1000;
+        
+        printf("[%5.2f%%] %llds\n", perc, sec);
+
         std::this_thread::sleep_for(1s);
     }
-    printf(" ");
+
+    auto end = chrono::steady_clock::now();
+    auto total_time = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+    printf("Done! (%lldms)\n", total_time);
 }
 
 
+static void init_random_scene()
+{
+    Material* ground_material = (Material*)malloc(sizeof(Material));
+    ground_material->type = Material_Type::lambertian;
+    ground_material->lambertian.color = color(0.5f, 0.5f, 0.5f);
+
+    {
+        Sphere s = sphere(vec3(0.0f, -1000.0f, 0.0f), 1000.0f, ground_material);
+        add_sphere_to_world(&global_world, &s);
+    }
+
+    for (int a = -11; a < 11; ++a) 
+    {
+        for (int b = -11; b < 11; ++b) 
+        {
+            float choose_mat = random_float();
+
+            Point3 center = vec3(
+                (float)a + 0.9f * random_float(), 
+                0.2f, 
+                (float)b + 0.9f * random_float());
+
+            if (length(center - vec3(4.0f, 0.2f, 0.0f)) > 0.9f)
+            {
+                if (choose_mat < 0.8f) 
+                {
+                    // diffuse
+                    Color albedo = random_color() * random_color();
+                    
+                    Material* sphere_material = (Material*)malloc(sizeof(Material));
+                    sphere_material->type = Material_Type::lambertian;
+                    sphere_material->lambertian.color = albedo;
+
+                    Sphere s = sphere(center, 0.2f, sphere_material);
+
+                    add_sphere_to_world(&global_world, &s);
+                }
+                else if (choose_mat < 0.95f) 
+                {
+                    // metal
+                    Color albedo = random_color(0.5f, 1.0f);
+                    float fuzz = random_float(0.0f, 0.5f);
+
+                    Material* sphere_material = (Material*)malloc(sizeof(Material));
+                    sphere_material->type = Material_Type::metal;
+                    sphere_material->metal.color = albedo;
+                    sphere_material->metal.fuzz = fuzz;
+
+                    Sphere s = sphere(center, 0.2f, sphere_material);
+
+                    add_sphere_to_world(&global_world, &s);
+                }
+                else 
+                {
+                    // glass
+                    Material* sphere_material = (Material*)malloc(sizeof(Material));
+                    sphere_material->type = Material_Type::dielectric;
+                    sphere_material->dielectric.ir = 1.5f;
+
+                    Sphere s = sphere(center, 0.2f, sphere_material);
+
+                    add_sphere_to_world(&global_world, &s);
+                }
+            }
+        }
+    }
+
+    {
+        Material* material1 = (Material*)malloc(sizeof(Material));
+        material1->type = Material_Type::dielectric;
+        material1->dielectric.ir = 1.5f;
+
+        Sphere s = sphere(vec3(0.0f, 1.0f, 0.0f), 1.0f, material1);
+
+        add_sphere_to_world(&global_world, &s);
+    }
+
+    {
+        Material* material2 = (Material*)malloc(sizeof(Material));
+        material2->type = Material_Type::lambertian;
+        material2->lambertian.color = vec3(0.4f, 0.2f, 0.1f);
+
+        Sphere s = sphere(vec3(-4.0f, 1.0f, 0.0f), 1.0f, material2);
+
+        add_sphere_to_world(&global_world, &s);
+    }
+
+    {
+        Material* material3 = (Material*)malloc(sizeof(Material));
+        material3->type = Material_Type::metal;
+        material3->metal.color = vec3(0.7f, 0.6f, 0.5f);
+        material3->metal.fuzz = 0.0f;
+
+        Sphere s = sphere(vec3(4.0f, 1.0f, 0.0f), 1.0f, material3);
+
+        add_sphere_to_world(&global_world, &s);
+    }
+
+    int stop = 0;
+}
 
 int main(void)
 {
@@ -158,52 +297,31 @@ int main(void)
 
     // Image 
 
-    float aspect_ratio = 16.0f / 9.0f;
-    int image_width = 480; // 1920 * 2;
+    float aspect_ratio = 3.0f / 2.0f;
+    int image_width = 240;
     int image_height = (int)((float)image_width / aspect_ratio);
-    int samples_per_pixel = 100;
-    int max_depth = 50;
+    int samples_per_pixel = 50;
+    int max_depth = 25;
     
-#define GOTTA_GO_FAST 0
+#define GOTTA_GO_SLOW 0
 
-#if GOTTA_GO_FAST
-    samples_per_pixel = 50;
-    max_depth = 25;
+#if GOTTA_GO_SLOW
+    image_width = 1200;
+    image_height = (int)((float)image_width / aspect_ratio);
+    samples_per_pixel = 500;
+    max_depth = 50;
 #endif
 
-    Material material_ground = {};
-    material_ground.type = Material_Type::lambertian;
-    material_ground.lambertian.color = color(0.8f, 0.8f, 0.0f);
-
-    Material material_center = {};
-    material_center.type = Material_Type::lambertian;
-    material_center.lambertian.color = color(0.1f, 0.2f, 0.5f);
-
-    Material material_left = {};
-    material_left.type = Material_Type::dielectric;
-    material_left.dielectric.ir = 1.5f;
-    
-    Material material_right = {};
-    material_right.type = Material_Type::metal;
-    material_right.metal.color = color(0.8f, 0.6f, 0.2f);
-    material_right.metal.fuzz = 0.0f;
-
     // World 
-    World world = {};
-
-    world.spheres[0] = sphere(vec3(0.0f, -100.5f, -1.0f), 100.0f, &material_ground);
-    world.spheres[1] = sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, &material_center);
-    world.spheres[2] = sphere(vec3(-1.0f, 0.0f, -1.0f), 0.5f, &material_left);
-    world.spheres[3] = sphere(vec3(-1.0f, 0.0f, -1.0f), -0.4f, &material_left);
-    world.spheres[4] = sphere(vec3(1.0f, 0.0f, -1.0f), 0.5f, &material_right);
+    init_random_scene();
 
     // Camera
 
-    Point3 lookfrom = vec3(3.0f, 3.0f, 2.0f);
-    Point3 lookat = vec3(0.0f, 0.0f, -1.0f);
+    Point3 lookfrom = vec3(13.0f, 2.0f, 3.0f);
+    Point3 lookat = vec3(0.0f, 0.0f, 0.0f);
     Vec3 vup = vec3(0.0f, 1.0f, 0.0f);
-    float dist_to_focus = length(lookfrom - lookat);
-    float aperture = 1.0f;
+    float dist_to_focus = 10.0f;
+    float aperture = 0.1f;
 
     Camera cam = camera(
         lookfrom, 
@@ -220,16 +338,14 @@ int main(void)
     fprintf(fp, "%d %d\n", image_width, image_height);
     fprintf(fp, "255\n");
 
-    printf("Begin rendering");
     std::thread progress_thread(print_progress);
-    chrono::time_point<chrono::steady_clock> start = chrono::steady_clock::now();
+    
+    g_total_scanlines = image_height;
 
     for (int row = image_height - 1;
         row >= 0;
         --row)
     {
-        //printf("Scanlines remaining: %d\n", row);
-
         for (int col = 0;
             col < image_width;
             ++col)
@@ -245,21 +361,18 @@ int main(void)
             
                 Ray ray = get_ray_from_camera(&cam, u, v);
 
-                pixel_color += ray_color(&ray, &world, max_depth);
+                pixel_color += ray_color(&ray, &global_world, max_depth);
             }
             
             write_color(fp, pixel_color, samples_per_pixel);
         }
-    }
 
-    auto end = chrono::steady_clock::now();
-    auto total_time = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+        ++g_scanlines_processed;
+    }
     
-    done_rendering = true;
+    g_done_rendering = true;
     if (progress_thread.joinable())
         progress_thread.join();
-
-    cout << "Done! (" << total_time <<"ms)" << endl;
 
     fclose(fp);
 
--- src/rtweekend.h
+++ src/rtweekend.h
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <stdlib.h>
 
+#include "vec3.h"
+
 const float INF = INFINITY;
 const float PI = 3.141592653f;
 
@@ -21,6 +23,28 @@ inline float random_float(float min, float max)
 {
     // return random float in range [min, max)
     return min + (max - min) * random_float();
+}
+
+inline Color random_color()
+{
+    Color color = {};
+
+    color.r = random_float();
+    color.g = random_float();
+    color.b = random_float();
+
+    return color;
+}
+
+inline Color random_color(float min, float max)
+{
+    Color color = {};
+
+    color.r = min + (max - min) * random_float();
+    color.g = min + (max - min) * random_float();
+    color.b = min + (max - min) * random_float();
+
+    return color;
 }
 
 inline float clamp(float x, float min, float max) 
